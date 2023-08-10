b5d1ab78971d99cae171ed286e04fca97f4e70f8
A-Mazurenko <oleksandr.mazurenko@globallogic.com>
2023-08-10 20:02:30 +0300

    Додано listener та тост з повідомленням

--- app/src/main/java/com/example/githelloworld/MainActivity.kt
+++ app/src/main/java/com/example/githelloworld/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.githelloworld
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_layout)
+        val button:Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            Toast.makeText(this, "Button is pressed", Toast.LENGTH_SHORT).show()
+        }
     }
 }
